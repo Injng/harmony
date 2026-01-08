a604e05ac1ca1782a8fb6f18aa510478bdfccc98
Lin Jiang <jianglin@lnjng.com>
2026-01-08 13:35:39 -0800

    feat: move server variables to configuration system

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use axum::{Router, routing::get};
+mod settings;
 
-const ADDRESS: &str = "0.0.0.0:3000";
+use axum::{Router, routing::get};
+use settings::Settings;
 
 async fn hello() -> String {
     String::from("Hello, world!")
@@ -8,10 +9,15 @@ async fn hello() -> String {
 
 #[tokio::main]
 async fn main() {
+    // load server configuration
+    let settings: Settings = Settings::load("./config/development.toml");
+    let address = format!("{}:{}", settings.server.host, settings.server.port);
+
+    // set up API routing and serve
     let router = Router::new().route("/", get(hello));
-    let listener = tokio::net::TcpListener::bind(ADDRESS)
+    let listener = tokio::net::TcpListener::bind(&address)
         .await
-        .expect(&format!("[FATAL] Failed to bind listener to {}", ADDRESS));
+        .expect(&format!("[FATAL] Failed to bind listener to {}", &address));
     axum::serve(listener, router)
         .await
         .expect("[FATAL] Failed to serve application");
---
+++ src/settings.rs
@@ -0,0 +1,27 @@
+use std::path::Path;
+
+use config::Config;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub server: ServerConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ServerConfig {
+    pub host: String,
+    pub port: u16,
+}
+
+impl Settings {
+    pub fn load(path: &str) -> Self {
+        let settings = Config::builder()
+            .add_source(config::File::from(Path::new(path)))
+            .build()
+            .expect("[FATAL] Failed to read configuration file");
+        settings
+            .try_deserialize()
+            .expect("[FATAL] Failed to deserialize configuration")
+    }
+}
