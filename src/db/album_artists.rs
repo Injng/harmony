bb188d34ac4ec05d67ad948ea0955aa05580073d
Lin Jiang <jianglin@lnjng.com>
2026-01-11 17:28:43 -0800

    fix: junction tables need to have consistent uuid type

--- src/db/album_artists.rs
+++ src/db/album_artists.rs
@@ -5,9 +5,9 @@ use sea_orm::entity::prelude::*;
 #[sea_orm(table_name = "album_artists")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
-    pub album_id: i32,
+    pub album_id: Uuid,
     #[sea_orm(primary_key, auto_increment = false)]
-    pub artist_id: i32,
+    pub artist_id: Uuid,
     #[sea_orm(belongs_to, from = "album_id", to = "id")]
     pub album: Option<super::album::Entity>,
     #[sea_orm(belongs_to, from = "artist_id", to = "id")]
--- src/db/track_artists.rs
+++ src/db/track_artists.rs
@@ -5,9 +5,9 @@ use sea_orm::entity::prelude::*;
 #[sea_orm(table_name = "track_artists")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
-    pub track_id: i32,
+    pub track_id: Uuid,
     #[sea_orm(primary_key, auto_increment = false)]
-    pub artist_id: i32,
+    pub artist_id: Uuid,
     #[sea_orm(belongs_to, from = "track_id", to = "id")]
     pub track: Option<super::track::Entity>,
     #[sea_orm(belongs_to, from = "artist_id", to = "id")]
